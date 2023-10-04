8df37c0a568ed3585ad7b13a3d9ea6823d6ab857
b0korpat <b0korpat@gmail.com>
2023-10-04 21:12:02 +0200

    kész projekt feladat

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -19,17 +19,12 @@ darkModeToggle.addEventListener('change', () => {
             element.style.border = '3px white solid';
         });
 
-        darkModeIcon.classList.add('spin');
         darkModeIcon.src = 'img/sun.png';
-
-      
-
     } else {
 
         mainDiv.style.backgroundColor = '';
         mainDiv.style.color = 'black';
         darkModeLabel.style.color = 'black';
-        
 
         borders.forEach(element => {
             element.style.border = '2px black solid';
@@ -39,9 +34,7 @@ darkModeToggle.addEventListener('change', () => {
             element.style.border = '3px black solid';
         });
 
-        darkModeIcon.classList.add('spin');
         darkModeIcon.src = 'img/moon.png';
 
-        darkModeIcon.classList.remove('spin');
     }
 });
