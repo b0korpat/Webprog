8731d9512895d612c8ac3689ae19ef7be03092b1
2020b-Kordás Patrik <b0korpat@vasvari.org>
2023-12-08 11:32:15 +0100

    save

--- Project2/js/script.js
+++ Project2/js/script.js
@@ -2,6 +2,56 @@ const gameContainer = document.body
 const menuElement = document.getElementsByClassName('menu')
 const game = document.getElementById('game')
 const cardsDiv = document.getElementById('cards')
+const stopwatch = document.getElementById('stopwatch')
+const highestscore = "00:00:00"
+var currentTime
+document.getElementById('highestScore').innerHTML = highestscore
+
+let stopwatchInterval;
+let seconds = 0, minutes = 0, hours = 0;
+
+function startStopwatch() {
+  stopwatchInterval = setInterval(updateStopwatch, 1000);
+}
+
+function stopStopwatch() {
+  clearInterval(stopwatchInterval);
+}
+
+function resetStopwatch() {
+  clearInterval(stopwatchInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  updateStopwatch();
+}
+
+function updateStopwatch() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  const formattedTime = pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+  currentTime = formattedTime; 
+  document.getElementById('stopwatch').innerText = formattedTime;
+}
+
+function pad(value) {
+  return value < 10 ? '0' + value : value;
+}
+
+
+
+
+
+
+
 
 function toggleFullscreen() {
   if (document.fullscreenElement) {
@@ -18,8 +68,8 @@ function toggleFullscreen() {
 }
 
 function menuHide() {
+  startStopwatch();
   const menuElements = document.getElementsByClassName("menu");
-
   Array.from(menuElements).forEach(function (menuElement) {
     menuElement.classList.add("hidden");
     setTimeout(function () {
@@ -33,12 +83,13 @@ function menuHide() {
 function gameStart() {
   game.style.display = "flex";
   cardsDiv.style.display = "grid";
+  stopwatch.style.display = "block";
   let cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
   shuffle(cards)
   let out = "";
 
   for (let i = 0; i < 12; i++) {
-    out += `<div class="card-container" onclick="flipCard(this)" data-value="${cards[i]}" data-index="${i}"><div class="card"><div class="card-face front">XD</div><div class="card-face back">${cards[i]}</div></div></div>`
+    out += `<div class="card-container" onclick="flipCard(this)" data-value="${cards[i]}" data-index="${i}" data-flipped="0"><div class="card"><div class="card-face front"></div><div class="card-face back">${cards[i]}</div></div></div>`
   }
 
   cardsDiv.innerHTML = out;
@@ -67,56 +118,86 @@ let flipCardsValue = []
 
 function flipCard(cardContainer) {
   const card = cardContainer.querySelector('.card');
-  card.classList.toggle('flipped');
+  if (cardContainer.dataset.flipped == 0) {
 
-  cards.push(card)
-  flipCardsValue.push(cardContainer.dataset.value)
+    cardContainer.dataset.flipped = 1
 
-
-  console.log(flipCardsValue)
-  console.log(score)
+    card.classList.toggle('flipped');
 
 
+    cards.push(card)
+    flipCardsValue.push(cardContainer.dataset.value)
 
-  if (flipCardsValue.length >= 2) {
-    if (flipCardsValue[0] === flipCardsValue[1]) {
-      console.log("jo")
-      console.log(flipCardsValue)
-      console.log(score)
-      score++;
 
-      cards[0].style.animation = "dim 1s";
-      cards[1].style.animation = "dim 1s";
-      
+    console.log(flipCardsValue)
+    console.log(score)
+
+
+
+    if (flipCardsValue.length >= 2) {
       setTimeout(() => {
-      cards[0].style.display = "none"
-      cards[1].style.display = "none"
-        flipCardsValue.splice(0, flipCardsValue.length);
-        cards.splice(0, cards.length);
-      }, 500);
-    } else {
-      console.log("nem jo")
-      console.log(flipCardsValue)
-  console.log(score)
+        document.querySelectorAll('.card-container').forEach(element => {
+          element.dataset.flipped = 1;
+        });
+        if (flipCardsValue[0] === flipCardsValue[1]) {
+          console.log("jo")
+          console.log(flipCardsValue)
+          console.log(score)
+          score++;
 
-      setTimeout(() => {
-        for (let j = 0; j < cards.length; j++){
-          cards[j].classList.toggle('flipped');
+
+
+
+          cards[0].parentNode.style.opacity = 0;
+          cards[1].parentNode.style.opacity = 0;
+
+          setTimeout(() => {
+            cards[0].style.display = "none"
+            cards[1].style.display = "none"
+            flipCardsValue.splice(0, flipCardsValue.length);
+            cards.splice(0, cards.length);
+            document.querySelectorAll('.card-container').forEach(element => {
+              element.dataset.flipped = 0;
+            });
+          }, 500);
+
+        } else {
+          console.log("nem jo")
+          console.log(flipCardsValue)
+          console.log(score)
+
+          setTimeout(() => {
+            for (let j = 0; j < cards.length; j++) {
+              cards[j].classList.toggle('flipped');
+            }
+
+            flipCardsValue.splice(0, flipCardsValue.length);
+            cards.splice(0, cards.length);
+
+            document.querySelectorAll('.card-container').forEach(element => {
+              element.dataset.flipped = 0;
+            });
+          }, 500);
+
+
+
+
         }
-        
-        flipCardsValue.splice(0, flipCardsValue.length);
-        cards.splice(0, cards.length);
       }, 700);
-
-
     }
   }
 
-
-
-  if(score >= 6){
+  if (score >= 6) {
     setTimeout(() => {
+      stopStopwatch();
+      if (highestscore < currentTime) {
+        highestscore = currentTime;
+
+      }
       alert("Nyertél!")
     }, 2000);
   }
 }
+
+
+
