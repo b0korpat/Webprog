049181330758802f017f2f46bd84d4a07362165d
2020b-Kordás Patrik <b0korpat@vasvari.org>
2024-01-12 11:25:20 +0100

    save

---
+++ nyíl/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener('click', () => {
+    document.getElementById('output').value = document.getElementById('input').value.split("").reverse().join("")
+})
+
+
+
+
