fe48d9eab1c25dcbe809e67310451b9462187355
2020b-Kordás Patrik <b0korpat@vasvari.org>
2024-02-02 10:18:48 +0100

    asd

---
+++ insert_dash/insertDash.js
@@ -0,0 +1,7 @@
+function insertDash(num) {
+  num = num.toString().split('')
+   for(let i = 0; i < num.length; i++){
+    num[i]%2 > 0 && num[i +1]%2 > 0 ? num[i] = num[i] + '-' : ""
+   }
+   return num.join('') 
+ }
