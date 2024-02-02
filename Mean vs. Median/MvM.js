039ab7527fb7e41d4d0150774f4d95e26c1d3128
2020b-Kordás Patrik <b0korpat@vasvari.org>
2024-02-02 09:50:50 +0100

    save

---
+++ Mean vs. Median/MvM.js
@@ -0,0 +1,13 @@
+function meanVsMedian(numbers) {
+    let mean = numbers.reduce((x, y) => x + y) / numbers.length;
+    numbers.sort
+    let median = numbers[Math.floor(numbers.length / 2)]
+    console.log(mean)
+    console.log(median)
+    console.log(mean === median ? "same" : mean > median ? "mean" : "median")
+    return mean == median ? "same" : mean > median ? "mean" : "median"
+}
+
+meanVsMedian([1, 1, 1])
+meanVsMedian([1, 2, 37])
+meanVsMedian([7, 14, -70])
