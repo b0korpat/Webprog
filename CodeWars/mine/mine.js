6a3a6558228a553b9e821836f3bc15049b0aa7dc
2020b-Kordás Patrik <b0korpat@vasvari.org>
2024-05-15 10:03:43 +0200

    s

---
+++ CodeWars/mine/mine.js
@@ -0,0 +1,11 @@
+function mineLocation(field) {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j] == 1) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+console.log(mineLocation([[1, 0, 0], [0, 0, 0], [0, 0, 0]]));
