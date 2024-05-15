ec2e3b6e80e39def6787eabda87f4295d8e2625d
2020b-Kordás Patrik <b0korpat@vasvari.org>
2024-05-15 09:57:51 +0200

    fájl rendezes

---
+++ Project/Project 1/js/script.js
@@ -0,0 +1,40 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+const darkModeIcon = document.getElementById('darkModeIcon');
+const darkModeLabel = document.getElementById('darkModeLabel');
+const mainDiv = document.querySelector('.orarend');
+const borders = document.querySelectorAll('hr, h1, .ora');
+const bordersForH2 = document.querySelectorAll('h2');
+
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        mainDiv.style.backgroundColor = 'black';
+        mainDiv.style.color = 'white';
+        darkModeLabel.style.color = 'white';
+
+        borders.forEach(element => {
+            element.style.border = '2px white solid';
+        });
+
+        bordersForH2.forEach(element => {
+            element.style.border = '3px white solid';
+        });
+
+        darkModeIcon.src = 'img/sun.png';
+    } else {
+
+        mainDiv.style.backgroundColor = '';
+        mainDiv.style.color = 'black';
+        darkModeLabel.style.color = 'black';
+
+        borders.forEach(element => {
+            element.style.border = '2px black solid';
+        });
+
+        bordersForH2.forEach(element => {
+            element.style.border = '3px black solid';
+        });
+
+        darkModeIcon.src = 'img/moon.png';
+
+    }
+});
